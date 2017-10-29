c8160fa1f6b9439b93224e4329fd6caf4fae07a0
somil kumar <somil.kumar91@gmail.com>
2017-10-29 19:36:29 +0530

    Adding class component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi!</div>;
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyDJm4tctOf4GAHy5bEMu0rIrPpm4mbOM_s';
+
+const App = () => {
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    ) ;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
